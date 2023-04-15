e0521248f936fa63329206f938555c3d6af07854
Rashed967 <rashedhasan967.com>
2023-04-15 22:51:05 +0600

    added some code

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,26 +1,65 @@
-import React from 'react';
-import {GoogleAuthProvider, getAuth, signInWithPopup} from "firebase/auth";
+import React, { useState } from 'react';
 import app from '../../firebase/firebase.init';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
+
 
 const Login = () => {
-
+    const [user, setUser] = useState()
     const auth = getAuth(app)
-    console.log(app)
-    const provider = new GoogleAuthProvider()
+    const googleProvider = new GoogleAuthProvider()
+    const githubProvider = new GithubAuthProvider()
+    
     const googleSignIn = () => {
-        signInWithPopup(auth,provider)
-        .then((result) => {
-            const user = result.user
-
+        signInWithPopup(auth, googleProvider)
+        .then(result => {
+            const loggedIn = result.user
+            setUser(loggedIn)
+            console.log(user)
         })
-        .catch((error) => {
-            console.log("error", error.message)
+        .catch(error => {
+            console.log("Error", error.message)
         })
     }
+
+    const githubSignIn = () =>{
+        signInWithPopup(auth, githubProvider)
+        .then(result => {
+            const loggedInUser = result.user
+            setUser(loggedInUser)
+            console.log(loggedInUser)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }
+
+    const signOutHandle = () => {
+    signOut(auth)
+    .then(result => {
+        setUser(null)
+    })
+    .catch(error => {
+        console.log(error)
+    }) 
+}
+
+
     return (
         <div>
             <h2>Login page</h2>
-            <button onClick={googleSignIn}>Login</button>
+            { user ?
+                <button onClick={signOutHandle}>Sign out</button> :
+                <>
+                <button onClick={googleSignIn}>Login</button> 
+                <button onClick={githubSignIn}>Github login</button> 
+                </>
+            }
+           {user &&
+             <div>
+             <h3>{user.displayName}</h3>
+             <p>{user.email}</p>
+         </div>
+           }
         </div>
     );
 };
--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,6 +1,9 @@
-
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCeZXfQLMr03N1lN0M-Z0G0C_uFqrh42OA",
   authDomain: "siimple-easy-firebase.firebaseapp.com",
@@ -12,5 +15,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export default app
